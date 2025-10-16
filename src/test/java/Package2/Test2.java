cadb841e5a2c5fcbe9fa4551e56a619394995b6c
satyam <satyam.dubey@fleekitsolutions.com>
2025-10-16 16:23:30 +0530

    this is for practice

--- src/test/java/Package2/Test2.java
+++ src/test/java/Package2/Test2.java
@@ -3,6 +3,8 @@ package Package2;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("This is Radha");
+		System.out.println("This is ......");
+		System.out.println("...........");
 	}
 
 }
